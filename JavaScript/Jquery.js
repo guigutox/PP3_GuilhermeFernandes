8150caa3e7d37b8c6d2bd55951988c9d66b11324
Guilherme Augusto Fernandes <guilhermegutofer@gmail.com>
2023-01-10 16:29:37 -0300

    Botão para aumentar e diminuir imagem com jquery
    Adicionado botão  na pagina index para aumentar e diminuir a imagem do panda.

---
+++ JavaScript/Jquery.js
@@ -0,0 +1,14 @@
+$('#btn_aumentar').click(
+
+    function(){
+        $('#pandaIMG').animate({width:"800px"})
+    }
+)
+
+
+$('#btn_diminuir').click(
+
+    function(){
+        $('#pandaIMG').animate({width:"400px"})
+    }
+)
